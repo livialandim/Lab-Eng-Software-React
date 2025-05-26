a1c464b8f84c8b3aa78ac6cd13aed9baf2a0b6d0
Lívia Landim <livialandim04@gmail.com>
2025-05-26 14:58:03 -0300

    Atualização da estrutura dos arquivos e do contador

---
+++ src/Atividade02/Contador.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+
+
+export default function Contador() {
+    const [homem, setHomem] = useState(0);
+    const [mulher, setMulher] = useState(0);
+    const total = homem + mulher;
+
+    function AdicionarH() {
+        setHomem(homem + 1);
+    };
+
+    function SubtrairH() {
+        if (homem > 0)
+            setHomem(homem - 1);
+    };
+
+    function AdicionarM() {
+        setMulher(mulher + 1);
+    }
+
+    function SubtrairM() {
+        if (mulher > 0)
+            setMulher(mulher - 1);
+    }
+
+    function Resetar() {
+        setHomem(0);
+        setMulher(0);
+    }
+
+    return (
+        <div className="contador">
+            <div className="total-container">
+                <p className="total-label">Total</p>
+                <button onClick={Resetar} title="Resetar">🔄</button>
+            </div>
+            <p className="total-numero">{total}</p>
+
+            <div className="pessoas-container">
+                <div className="card">
+                    <img src="https://cdn-icons-png.flaticon.com/512/1995/1995574.png" alt="Homem" />
+                    <div className="card-title">Homens</div>
+                    <div className="botoes">
+                        <button onClick={AdicionarH}>➕</button>
+                        <button onClick={SubtrairH}>➖</button>
+                    </div>
+                    <div className="contagem">{homem}</div>
+                </div>
+
+                <div className="card">
+                    <img src="https://cdn-icons-png.flaticon.com/512/1995/1995522.png" alt="Mulher" />
+                    <div className="card-title">Mulheres</div>
+                    <div className="botoes">
+                        <button onClick={AdicionarM}>➕</button>
+                        <button onClick={SubtrairM}>➖</button>
+                    </div>
+                    <div className="contagem">{mulher}</div>
+                </div>
+            </div>
+        </div>
+    );
+
+}
--- src/minhasRotas.js
+++ src/minhasRotas.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./Home";
-import Atividade1 from "./Atividade1";
-import Atividade2 from "./Atividade2";
+import Atividade1 from "./Atividade01/Atividade1";
+import Atividade2 from "./Atividade02/Atividade2";
 import Atividade3 from "./Atividade03/Atividade3";
 
 export default function MinhasRotas() {
     return (
-        // Todas as rotas devem estar aqui dentro do BrowserRouter
+
         <BrowserRouter>
             <Routes>
                 <Route path = "/" element = {<Home />} />
