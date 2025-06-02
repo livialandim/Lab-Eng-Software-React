b3d847b282be915bcec1917f7f9e187b7f191f07
Lívia Landim <livialandim04@gmail.com>
2025-06-02 19:18:43 -0300

    Atividade 5

--- src/Atividade05/Atividade5.js
+++ src/Atividade05/Atividade5.js
@@ -1,5 +1,9 @@
 import { Link } from "react-router-dom";
-
+import FormQuiz from "./FormQuiz";
+import FormTicket from "./FormTicket";
+import Accordion from "./Accordion";
+import ContactList from "./ContactList";
+import Chat from "./Chat";
 
 export default function Atividade5() {
     return (
@@ -8,8 +12,18 @@ export default function Atividade5() {
 
             <h1>Semana 5</h1>
 
-            <h2>Contador</h2>
+            <h2>City Quiz</h2>
+            <FormQuiz />
 
+            <h2>FormTicket</h2>
+            <FormTicket />
+
+            <h2>Accordion</h2>
+            <Accordion />
+
+            <h2>ContactList e Chat</h2>
+            <ContactList/>
+            <Chat />
         </>
     );
 }
