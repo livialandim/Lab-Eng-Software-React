e32532f314b8e53cebfaec114a9813b72f29be65
livialandim <livialandim04@gmail.com>
2025-06-03 20:09:30 -0300

    Atualização do arquivo Atividade5.js

--- src/Atividade05/Atividade5.js
+++ src/Atividade05/Atividade5.js
@@ -1,19 +1,11 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
 import FormQuiz from "./FormQuiz";
 import FormTicket from "./FormTicket";
 import Accordion from "./Accordion";
-import ContactList from "./ContactList";
-import Chat from "./Chat";
+
+import Messenger from "../App";
 
 export default function Atividade5() {
-    const [selectedContact, setSelectedContact] = useState(null);
-    const contacts = [
-        { name: 'Ana', email: 'ana@email.com' },
-        { name: 'Beatriz', email: 'beatriz@email.com' },
-        { name: 'Carolina', email: 'carolina@email.com' }
-    ]
-
     return (
         <>
             <Link to="/">Voltar</Link>
@@ -30,12 +22,7 @@ export default function Atividade5() {
             <Accordion />
 
             <h2>ContactList e Chat</h2>
-            <ContactList
-                contacts={contacts}
-                selectedContact={selectedContact}
-                onSelect={setSelectedContact}
-            />
-            <Chat contact={selectedContact} />
+            <Messenger />
         </>
     );
 }
